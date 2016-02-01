15efcf9218002ed40c9eaeeb5b1af32130556115
aifei <aifei@360.cn>
2016-02-01 14:41:57 +0800

    不一定需要动态加载

--- src/main.lua
+++ src/main.lua
@@ -1,22 +1,21 @@
 local router = require 'resty.router'
 
-local cache = ngx.shared.cache_ngx
+--  如果需要启动代码动态加载 开启注释中的代码 并对 get_code_chunk 进行编码 
+-- local cache = ngx.shared.cache_ngx
+-- local redis = require "src.comm.redis-plus"
+-- local red = redis:new()
 
+-- local function get_code_chunk(opt)
+--     -- body
+--     local code_chunk, err = red:get("some_key")
+--     return code_chunk
+-- end
 
-local redis = require "src.comm.redis-plus"
-local red = redis:new()
+-- local opt = { func = get_code_chunk }
 
+-- local rt = router:new(cache, opt)
 
-local function get_code(opt)
-    -- body
-    local s, err = red:get("some_key")
-    return s
-end
-
-local opt = { func = get_code }
-
-local rt = router:new(cache, opt)
--- local rt = router:new()
+local rt = router:new()
 
 rt:map('/api/hello.json', 'src.hello')
 
